77c76eef10904a47907e416c541cfc3e6deaa52c
Faisalkh25 <faisalkhbla25@gmail.com>
2026-02-22 15:47:27 +0530

    first commit

---
+++ core_01/lect_01.py
@@ -0,0 +1,31 @@
+
+a=2
+b="faisal"
+
+print(a, b)
+# python datatypes (5)
+# int, floating point numbers, string, boolean, none
+
+# function type and typecasting
+
+a="21.4"
+b=float(a)
+c=type(b)
+print(c)
+
+
+
+# input
+# a = input("enter the number : ")           #input takes string
+# b = input("enter the second number : ")
+
+# print(a + b) 
+
+# to make addition of these two numbers you need to typecast
+a = int(input("enter the number: "))
+b = int(input("enter the second number: "))
+
+print(a + b)
+
+
+
